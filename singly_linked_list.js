7f8ab30ed1ab2e33ca58c4fd1e2f9de2870070ec
David Weatherspoon <david_weatherspoon@hotmail.com>
2020-10-31 19:37:45 +0000

    Added functionality to linked lists including shift, unshift, get, set & insert

--- singly_linked_list.js
+++ singly_linked_list.js
@@ -50,7 +50,72 @@ class SinglyLinkedList {
             return temp;
         }
     }
+
+    shift() {
+        // empty list
+        if (this.length === 0) {
+            return undefined; 
+        } else {
+             // sever head, reset head & return original
+            let temp = this.head;
+            this.head = this.head.next;
+            this.length --;
+            if (this.length === 0) {
+                this.tail = null;
+                return temp.val;
+            }
+        }
+    }
+
+    unshift(val) {
+        // create new node with val
+        let newNode = new Node(val);
+        // if no head set head & tail to new node
+        if (!this.head && !this.tail) {
+            this.head = newNode;
+            this.tail = this.head;
+        // else set new node as head
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length ++;
+        return this;
+    }
+
+    get(index) {
+        // special cases
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.head;
+        // traverse list
+        let current = this.head
+        for(let count=0; count<index; count++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    set(index, val) {
+        let reqNode = this.get(index);
+        if (reqNode === undefined) return false;
+        reqNode.val = val;
+        return true;
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(val); // !! ensures true is returned
+        if (index === this.length) return !!this.push(val); // !! ensures true is returned
+        let previousNode = this.get(index - 1);
+        let newNode = new Node(val);
+        newNode.next = previousNode.next;
+        previousNode.next = newNode;
+        this.length ++;
+        return true;
+    }
 }
 
-
-var list = new SinglyLinkedList()
+let myList = new SinglyLinkedList()
+myList.push(10);
+myList.push(20);
+myList.push(30);
