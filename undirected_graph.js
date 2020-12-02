935cf3a1d29c00551f29c4953bd7772e091269b4
David Weatherspoon <david_weatherspoon@hotmail.com>
2020-12-02 23:14:01 +0000

    Added recursive depth first search functionality to undirected graph

--- undirected_graph.js
+++ undirected_graph.js
@@ -40,11 +40,47 @@ class UndirectedGraph {
         delete this.adjacencyList[vertex];
         return 0;
     }
+
+    DFSRecursive(startNode) {
+        let result = [];
+        let visited = {};
+        // line required as meaning of this changes in helper DFS func
+        let adjacencyList = this.adjacencyList;
+        function DFS (node) {
+            if (!node) return null;
+            if (!visited[node]) {
+                visited[node] = true;
+                result.push(node);
+            }
+            for (let item of adjacencyList[node]) {
+                if (!visited[item]) DFS(item);
+            }
+            return null;
+        }
+        DFS(startNode);
+        return result;
+    }
 }
 
-let a = new UndirectedGraph;
-a.addVertex('Paris');
-a.addVertex('London');
-a.addVertex('Barcelona');
-a.addEdge('Paris', 'London');
-a.addEdge('Paris', 'Barcelona');
+let g = new UndirectedGraph;
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
+
+let loc = new UndirectedGraph;
+loc.addVertex('Paris');
+loc.addVertex('London');
+loc.addVertex('Barcelona');
+loc.addEdge('Paris', 'London');
+loc.addEdge('Paris', 'Barcelona');
